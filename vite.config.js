8448d48742bed9cc02989350406abedba51938ae
unknown <philip_gullberg@hotmail.com>
2023-11-25 15:45:19 +0100

    changed base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/AIkunskap/",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
